df284c2861e4e36518f411d34922f1f94510540c
GaluTi <timofei.galushkin@gmail.com>
2022-12-20 02:15:50 +0300

    UPD: added x_path list calculation and visualization function

--- golden_section.py
+++ golden_section.py
@@ -1,8 +1,11 @@
+import matplotlib.pyplot as plt
+from matplotlib.animation import PillowWriter
+import numpy as np
 import math
 from minimizer import Minimizer
 
 class GoldenSection(Minimizer):
-    def __init__(self, eps=1e-10, n=900):
+    def __init__(self, eps=1e-10, n=900, x_path = []):
         '''
         Golden section optimizer
 
@@ -10,13 +13,17 @@ class GoldenSection(Minimizer):
         ---------
         eps: minimal distance to minumum
         n: maximum iteration
+        x_path: list of pairs [x1_i, x2_i], where x1_i and x2_i is a points of golden ratio on i-th iteration
         '''
         self.eps = eps
         self.n = n
-    def _golden_section(self, func, a1, b1, itera, eps, n): 
+        self.x_path = x_path
+
+    def _golden_section(self, func, a1, b1, itera, eps, n, x_path): 
         x1 = a1 + (3 - math.sqrt(5))/2*(b1-a1) 
         x2 = a1 + (math.sqrt(5) - 1)/2*(b1-a1)
-
+        x_path.append([x1, x2])
+    
         m_cond = func(x1) <= func(x2)
 
         if itera == n:
@@ -33,7 +40,8 @@ class GoldenSection(Minimizer):
             a2 = x1
             b2 = b1
             s_x2 = x2
-        return self._golden_section(func, a2, b2, itera+1, eps, n)
+        return self._golden_section(func, a2, b2, itera+1, eps, n, x_path)
+
     def minimize(self, func, a1, b1):
         '''
         Golden section optimizer method
@@ -46,9 +54,11 @@ class GoldenSection(Minimizer):
         '''
         eps = self.eps
         n = self.n
+        x_path = self.x_path
         x1 = a1 + (3 - math.sqrt(5))/2*(b1-a1)
         x2 = a1 + (math.sqrt(5) - 1)/2*(b1-a1)
-        
+        x_path.append([x1, x2])
+
         if func(x1) <= func(x2):
             a2 = a1
             b2 = x2
@@ -57,4 +67,28 @@ class GoldenSection(Minimizer):
             a2 = x1
             b2 = b1
             s_x2 = x2
-        return self._golden_section(func, a2, b2, 1, eps, n)
+
+        x = self._golden_section(func, a2, b2, 1, eps, n, x_path)
+        self.visualize_results(func, a1, b1, x_path)
+        return x 
+
+    def visualize_results(self, func, a, b, x_path):
+        xlist = np.linspace(a, b, 1000)
+        ylist = [func(x) for x in xlist]
+        
+        fig = plt.figure()
+        plt.xlim((min(xlist), max(xlist)))
+        plt.ylim((min(ylist), min(ylist) + (max(xlist) - min(xlist))))
+        l1, = plt.plot([], [],  marker="o", markersize=5, c="red")
+        l2, = plt.plot([], [],  marker="o", markersize=5, c="green")
+
+        metadata = dict(title="Movie")
+        writer = PillowWriter(fps=2, metadata=metadata)
+
+        with writer.saving(fig, "Golden section method.gif", dpi=300):
+            plt.plot(xlist, ylist, c='b')
+            for i in range(len(x_path)):
+                plt.title(f"{i} iteration")
+                l1.set_data(x_path[i][0], func(x_path[i][0]))
+                l2.set_data(x_path[i][1], func(x_path[i][1]))
+                writer.grab_frame()    
