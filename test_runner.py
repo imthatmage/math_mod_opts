d1a5da23a1dbfaa8c20b0734f24ed0e0acb1cad7
Almir <nurm.almir@gmail.com>
2022-12-21 17:46:48 +0300

    UPD: new test for conditional gradient

--- test_runner.py
+++ test_runner.py
@@ -5,11 +5,12 @@ from golden_section import GoldenSection
 from dividing_method import DividingMethod
 from gradient_projection import GradientProjectionMethod
 from broken_lines_method import BrokenLinesMethod
+from gradient_conditional import GradientConditionalMethod
 
 
 def test_func_add(x):
-    return (x[0] + 5)**2 + math.sin(10*(x[0] - np.pi/4)) + x[1]**2
-    #return (x[0] + 5)**2 + x[1]**2
+    #return (x[0] + 5)**2 + math.sin(10*(x[0] - np.pi/4)) + x[1]**2
+    return (x[0] + 5)**2 + x[1]**2
 
 def test_func_add1(x):
     return (x[0] + 5)**2 + math.sin(10*(x[0] - np.pi/4)) + x[1]**2 + (x[2] - 1)**2
@@ -20,16 +21,16 @@ gs_method = GoldenSection()
 ds_method = DividingMethod()
 gr_method = GradientMethod()
 gp_method = GradientProjectionMethod(area_name='sphere', R=4, C=[0, 0])
-bl_method = BrokenLinesMethod(n=100)
+bl_method = BrokenLinesMethod()
 
 #test_func = lambda x: x**2
 #test_func = lambda x : np.cos(x)
-test_func = lambda x: x**2 + np.sin(10*(x - np.pi/4))
+test_func = lambda x: x**2#   + np.sin(10*(x - np.pi/4))
 
 print(f"Dividing Method {ds_method.minimize(test_func, -5, 6)}")
 print()
-print(f"Golden section: {gs_method.minimize(test_func, -5, 6)}")
-print(f"BrokenLinesMethod: {bl_method.minimize(test_func, -5, 5, 2)}")
+# print(f"Golden section: {gs_method.minimize(test_func, -5, 6)}")
+# print(f"BrokenLinesMethod: {bl_method.minimize(test_func, -5, 5, 100, 2, 100)}")
 # print(f"Gradient method: {gradient_method(test_func, 1, [3], method_name='armicho')}")
 # print(f"Gradient method: {gradient_method(test_func, 1, [3], method_name='monotone')}")
 # print(f"Gradient method: {gradient_method(test_func, 1, [3], method_name='apriori')}")
@@ -40,18 +41,18 @@ print(f"BrokenLinesMethod: {bl_method.minimize(test_func, -5, 5, 2)}")
 
 print()
 
-for name in ['monotone', 'armicho', 'apriori']:
-    # gr_method = GradientMethod(method_name=name, n=1000, verbose=False, full_vis=False)
-    # print(f"Gradient method: {name}")
-    # print(f"Gradient method: {gr_method.minimize(test_func, 1, [3])}")
-    # gr_method = GradientMethod(method_name=name, n=1000, verbose=True, full_vis=True)
-    # print(f"Gradient method: {gr_method.minimize(test_func_add, 2, [2, 5])}")
-    # print()
-    gr_method = GradientMethod(method_name=name, n=1000, verbose=True, full_vis=True)
-    print(f"Gradient method: {gr_method.minimize(test_func_add1, 3, [2, 5, 3])}")
-    print()
+# for name in ['monotone', 'armicho', 'apriori']:
+#     # gr_method = GradientMethod(method_name=name, n=1000, verbose=False, full_vis=False)
+#     # print(f"Gradient method: {name}")
+#     # print(f"Gradient method: {gr_method.minimize(test_func, 1, [3])}")
+#     # gr_method = GradientMethod(method_name=name, n=1000, verbose=True, full_vis=True)
+#     # print(f"Gradient method: {gr_method.minimize(test_func_add, 2, [2, 5])}")
+#     # print()
+#     gr_method = GradientMethod(method_name=name, n=1000, verbose=True, full_vis=True)
+#     print(f"Gradient method: {gr_method.minimize(test_func_add1, 3, [2, 5, 3])}")
+#     print()
 
-print()
+# print()
 
 # for name in ['monotone', 'armicho', 'apriori']:
 #     gp_method = GradientProjectionMethod(method_name=name, area_name='sphere', R=2, C=0)
@@ -68,3 +69,13 @@ print()
 #     gp_method = GradientProjectionMethod(method_name=name, area_name='sphere', R=10, C=[-5, 2, 3], n=100, verbose=True, full_vis=True)
 #     print(f"GMP: {gp_method.minimize(test_func_add1, 3, [2, 5, 3])}")
 #     print()
+
+
+
+for name in ['monotone', 'armicho', 'apriori']:
+    gp_method = GradientConditionalMethod(method_name=name, area_name='sphere', R=2, C=0)
+    print(f"GMC: {name}")
+    print(f"GMC: {gp_method.minimize(test_func, 1, [3])}")
+    gp_method = GradientConditionalMethod(method_name=name, area_name='sphere', R=4, C=[0, 0], n=1000, verbose=True, full_vis=True)
+    print(f"GMC: {gp_method.minimize(test_func_add, 2, [4, 12])}")
+    print()
